3c26ca6c4ab8e7f2a28cbf882466fdfb82c2ec10
safdar211ali <safdar211@hotmail.com>
2024-10-26 15:08:46 +0500

    updated

--- tour_management/tour_management/doctype/hajj_booking/hajj_booking.js
+++ tour_management/tour_management/doctype/hajj_booking/hajj_booking.js
@@ -24,17 +24,12 @@ frappe.ui.form.on('Hajj Booking', {
     },
 
     advance_pkr: function (frm) {
-        calculateTotalPackage(frm);
+       calculateTotalPackage(frm);
     },
 
     discount_pkr: function (frm) {
-        calculateTotalPackage(frm);
+       calculateTotalPackage(frm);
     },
-    hajj_package: function (frm) {
-        var hajj_package_id = frm.doc.hajj_package;
-        get_hajj_package(frm, hajj_package_id);
-    }
-
 });
 
 function calculateTotalPackage(frm) {
@@ -61,53 +56,8 @@ function calculateTotalPackage(frm) {
         total_amount_pkr = cint(total_package_sar) * cint(exchange_rate);
         frm.set_value('total_amount_pkr', total_amount_pkr.toFixed(0));
     }
-    frm.set_value('balance_pkr', (cint(total_amount_pkr) - (cint(advance_pkr) + cint(discount_pkr))).toFixed(0));
+    frm.set_value('balance_pkr', (cint(total_amount_pkr)-(cint(advance_pkr)+cint(discount_pkr))).toFixed(0));
 
 }
 
 
-function get_hajj_package(frm, hajj_package_id) {
-    console.log(hajj_package_id);
-    if (hajj_package_id) {
-        // Clear existing data before adding new entries
-        frm.clear_table("hotels");
-        frm.clear_table("hajj_flight_detail");
-
-        frappe.call({
-            method: "tour_management.tour_management.doctype.utils.get_hajj_package.get_hajj_package",
-            args: {
-                hajj_package_id: hajj_package_id
-            },
-            callback: function (response) {
-                if (response.message.hajj_package.length > 0) {
-                    var packageData = response.message.hajj_package[0];
-                    frm.set_value('package_duration', packageData.term);
-                    frm.set_value('days', packageData.days);
-                }
-
-                if (response.message.hotel_detail) {
-                    response.message.hotel_detail.forEach(function (hotel) {
-                        let entry = frm.add_child("hotels");
-                        entry.city = hotel.city,
-                        entry.hotel = hotel.hotel,
-                        entry.nights = hotel.nights,
-                        entry.room_type = hotel.room_type,
-                        entry.meal = hotel.meal
-                    });
-                }
-
-                if (response.message.flight_detail) {
-                    response.message.flight_detail.forEach(function (flight) {
-                        var entry = frm.add_child("hajj_flight_detail");
-                        entry.type = flight.type,
-                        entry.cls = flight.cls,
-                        entry.airline = flight.airline
-                    });
-                }
-
-                frm.refresh_field('hotels');
-                frm.refresh_field('hajj_flight_detail');
-            }
-        });
-    }
-}
--- tour_management/tour_management/doctype/package_ticket/package_ticket.py
+++ tour_management/tour_management/doctype/package_ticket/package_ticket.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class PackageHotelDetail(Document):
+class PackageTicket(Document):
 	pass
